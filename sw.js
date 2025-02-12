215e8c91e8aa59b015410518c8f0302239f1d8a2
Shweta Kamble <imshweta49@gmail.com>
2025-02-12 21:16:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,3 +13,14 @@ self.addEventListener("fetch", (event) => {
         })
     );
 });
+self.addEventListener("install", (event) => {
+    console.log("Service Worker Installed");
+});
+
+self.addEventListener("activate", (event) => {
+    console.log("Service Worker Activated");
+});
+
+self.addEventListener("fetch", (event) => {
+    console.log("Fetching:", event.request.url);
+});
